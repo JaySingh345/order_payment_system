4ac8ecf79398f58b378514557abdb6b16d2303a7
jay singh <jaysingh94127@gmail.com>
2026-06-22 12:29:18 +0530

    ruff linted

--- starter.py
+++ starter.py
@@ -3,6 +3,7 @@ from temporalio.client import Client
 from workflow_service.workflows.order_workflow import OrderWorkflow
 from shared.models import Order
 
+
 async def main():
     client = await Client.connect("localhost:7233")
 
@@ -10,25 +11,17 @@ async def main():
         order_id=1,
         customer_id=101,
         amount=150.00,
-        items=[
-            {
-                "product_id" : 1,
-                "quantity" : 2
-            },
-            {
-                "product_id" : 2,
-                "quantity" : 2
-            }
-        ],
+        items=[{"product_id": 1, "quantity": 2}, {"product_id": 2, "quantity": 2}],
         address="Noida",
-        email="john@example.com"
+        email="john@example.com",
     )
     result = await client.execute_workflow(
         OrderWorkflow.run,
         order,
-        id = f"order-{order.order_id}",
-        task_queue = "order-task-queue"
+        id=f"order-{order.order_id}",
+        task_queue="order-task-queue",
     )
     print(result)
+
 
 asyncio.run(main())
