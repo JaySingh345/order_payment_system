538f1592dddaa11beed51feed8ddfd6ea71708db
jay singh <jaysingh94127@gmail.com>
2026-06-24 14:29:36 +0530

    Update order_workflow.py

--- workflow_service/workflows/order_workflow.py
+++ workflow_service/workflows/order_workflow.py
@@ -18,7 +18,7 @@ from temporalio.common import RetryPolicy
 class OrderWorkflow:
     def __init__(self):
         self.order_approved = False
-        
+
     @workflow.signal
     def approve_order(self):
         self.order_approved = True
@@ -28,13 +28,12 @@ class OrderWorkflow:
             activity,
             args=args,
             start_to_close_timeout=timedelta(seconds=10),
-
             retry_policy=RetryPolicy(
-                initial_interval = timedelta(seconds=5),
+                initial_interval=timedelta(seconds=5),
                 backoff_coefficient=2.0,
                 maximum_interval=timedelta(seconds=10),
-                maximum_attempts=5
-            )
+                maximum_attempts=5,
+            ),
         )
 
     @workflow.run
@@ -42,10 +41,8 @@ class OrderWorkflow:
 
         if not await self.execute(validate_order, order):
             return "Order validation failed"
-        
-        await workflow.wait_condition(
-            lambda: self.order_approved
-        )
+
+        await workflow.wait_condition(lambda: self.order_approved)
 
         transaction_id, amount = await self.execute(process_payment, order)
 
@@ -53,17 +50,15 @@ class OrderWorkflow:
         if not await self.execute(reserve_inventory, order):
             return "Inventory unavailable"
 
-        
         tracking_id = await self.execute(ship_order, order)
 
         order_id = await self.execute(save_order, order, transaction_id, tracking_id)
-        
-        await self.execute(send_email, order, tracking_id, transaction_id,order_id)
 
+        await self.execute(send_email, order, tracking_id, transaction_id, order_id)
 
         return {
             "status": "completed",
-            "order_id" : order_id,
+            "order_id": order_id,
             "transaction_id": transaction_id,
             "tracking_id": tracking_id,
         }
